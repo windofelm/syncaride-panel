f667a5629b6874d96b39d044ab012cbd260e2496
Windofelm <recep.hidir@gmail.com>
2015-12-22 13:56:26 +0200

    app

--- root/app.js
+++ root/app.js
@@ -1,2 +1,19 @@
 
-var app = angular.module('SyncaridePanel', []);
+var app = angular.module('SyncaridePanel', ['ngRoute']);
+
+
+app.config(function($routeProvider, $locationProvider){
+
+    $routeProvider
+        .when('/Sample', {
+
+            templateUrl: "templates/sample.html",
+            controller: "MainController"
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        })
+
+
+});
